2db5c224abca8ff924fa306280bb2f7e527882b2
alicew <wanderlust.alice00@gmail.com>
2022-05-04 12:59:31 +0200

    profile changed

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const profile = document.querySelector('.profile_container');
+const collections = document.querySelector('.user_collections');
+
+function addClass() {
+    profile.classList.add('fixed-position');
+    collections.classList.add('collections_margin')
+}
+
+function removeClass() {
+    profile.classList.remove('fixed-position');
+    collections.classList.remove('collections_margin')
+}
+
+window.addEventListener('scroll', (e) => {
+    let positionY = window.scrollY
+    if (positionY > 115) {
+        addClass()
+    } else if (positionY < 115) {
+        removeClass()
+    }
+})
