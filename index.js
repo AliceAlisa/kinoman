addeb2a8de9ea844b34414cf5d69c3e11fe29ed8
alicew <wanderlust.alice00@gmail.com>
2022-05-06 14:04:25 +0200

    profile style finished

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 'use strict';
 
-//const profile = document.querySelector('.profile_container');
 const profile = document.querySelector('.position_container');
-
 const collections = document.querySelector('.user_collections');
+
 
 function addClass() {
     profile.classList.add('fixed-position');
     collections.classList.add('collections_margin')
+
+    let newWidth = String(findwidthCollectionsBlock());
+    profile.style.width = newWidth + 'px';
 }
 
 function removeClass() {
     profile.classList.remove('fixed-position');
     collections.classList.remove('collections_margin')
+    profile.removeAttribute('style')
+}
+
+function findwidthCollectionsBlock() {
+    let width = collections.clientWidth
+    return width
 }
 
 window.addEventListener('scroll', (e) => {
     let positionY = window.scrollY
     if (positionY > 115) {
-        addClass()
+        if (profile.classList.contains('fixed-position')) {
+            return
+        } else {
+            addClass()
+        }
+
     } else if (positionY < 115) {
-        removeClass()
+        if (profile.classList.contains('fixed-position')) {
+            removeClass()
+        } else {
+            return
+        }
     }
 })
